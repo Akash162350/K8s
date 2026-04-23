c4cdfef083adb95e65e485719207b96cf3386af5
Akash G P <akashgpb02@gmail.com>
2026-04-23 16:19:25 +0530

    Update microk8s.sh

--- kubernetes-docs-main/microk8s.sh
+++ kubernetes-docs-main/microk8s.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 sudo snap install microk8s --classic --channel=1.35
+sudo usermod -a -G microk8s $USER
+mkdir -p ~/.kube
+chmod 0700 ~/.kube
 microk8s status
 microk8s start
 microk8s status
